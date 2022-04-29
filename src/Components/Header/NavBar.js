70b3c44e1da2f5f883b00678af23aed93fb44f9b
dlavrentev <dimitrilavrentev96@gmail.com>
2022-04-29 09:58:02 +0200

    tried to implement a navbar

---
+++ src/Components/Header/NavBar.js
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return (
+        <div className={classes.Navbar}>
+
+        </div>
+    )
+}
+
+export default NavBar;
---
+++ src/Components/Header/Navigation.js
@@ -0,0 +1,11 @@
+import NavLinks from './NavLinks'
+
+const Navigation = () => {
+    return ( 
+        <nav className={classes.Navigation}>
+        <NavLinks />
+        </nav>
+     );
+}
+
+export default Navigation;
