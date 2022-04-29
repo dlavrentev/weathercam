2b384d9df3fae698d41ba823560309ba4bb769b7
dlavrentev <dimitrilavrentev96@gmail.com>
2022-04-29 15:33:05 +0200

    Media query solution
    Webcam is loading and doesn't work.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,63 @@
-import React,{useContext} from 'react'
+import React,{useContext, Fragment} from 'react'
 import { ModeContext } from '../../Context/ModeContext'
 import profilepicture from '../../assets/profile.jpg'
+import Media from 'react-media';
+import { FaBars } from "react-icons/fa";
+
 
 export default function Header({user, toggleMode}) {
   
   const {darkMode, setDarkMode} = useContext(ModeContext)
   
   return (
-    <div className={darkMode ? "header-container header-container-dark" : "header-container"}>
-        <button className={darkMode ? "switch-btn switch-btn-dark" : "switch-btn"} onClick={toggleMode}>{darkMode ? "LIGHT" : "DARK"}</button>
-        <div className="nav-container">
+    <Media queries={{
+      small: "(max-width: 599px)",
+      medium: "(min-width: 600px) and (max-width: 1199px)",
+      large: "(min-width: 1200px)"
+    }}>
+      {matches => (
+        <Fragment>
+          {matches.small && <div className={darkMode ? "header-container header-container-dark" : "header-container"}>
+            <button className={darkMode ? "switch-btn switch-btn-dark" : "switch-btn"} onClick={toggleMode}>{darkMode ? "LIGHT" : "DARK"}</button>
+            <div>
+              <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/login"><FaBars size={50} /></a>
+            </div>
+            <div>
+                <img className="profile-image" src={profilepicture} alt="" />
+            </div>
             
-            <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/">Find</a>
-            <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/yourweather">Your weather</a>
-        </div>
-        <div>
-          <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/login">Login/Sign up</a>
-        </div>
-        <div>
-            <img className="profile-image" src={profilepicture} alt="" />
-        </div>
-        
-    </div>
+        </div>}
+          
+          {matches.medium && <div className={darkMode ? "header-container header-container-dark" : "header-container"}>
+            <button className={darkMode ? "switch-btn switch-btn-dark" : "switch-btn"} onClick={toggleMode}>{darkMode ? "LIGHT" : "DARK"}</button>
+            <div>
+              <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/login"><FaBars size={50}/></a>
+            </div>
+            <div>
+                <img className="profile-image" src={profilepicture} alt="" />
+            </div>
+            
+        </div>}
+          
+          {matches.large && <div className={darkMode ? "header-container header-container-dark" : "header-container"}>
+            <button className={darkMode ? "switch-btn switch-btn-dark" : "switch-btn"} onClick={toggleMode}>{darkMode ? "LIGHT" : "DARK"}</button>
+            <div className="nav-container">
+                
+                <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/">Find</a>
+                <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/yourweather">Your weather</a>
+            </div>
+            <div>
+              <a className={darkMode ? "a-links a-links-dark" : "a-links"} href="/login">Login/Sign up</a>
+            </div>
+            <div>
+                <img className="profile-image" src={profilepicture} alt="" />
+            </div>
+            
+        </div>}
+        </Fragment>
+      )}
+    </Media>
   )
 }
+
+
--- src/Components/MainContent/SearchLocation.js
+++ src/Components/MainContent/SearchLocation.js
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, Fragment } from "react";
 import axios from "axios";
+import Media from 'react-media';
 
 import { ModeContext } from '../../Context/ModeContext'
 
@@ -36,41 +37,83 @@ export default function SearchLocation() {
 
 
 return (
-    <div className={darkMode ? "searchlocations-container searchlocations-container-dark" : "searchlocations-container"}>
-    
-    
-      <div className="search-container">
-        
-        <h1 className={darkMode ? "weathercam-h weathercam-h-dark" : "weathercam-h"}>WEATHERCAM</h1>
-        <input id="searchlocations" className="searchlocations" type="text" placeholder="Search" aria-label="Search" value={location} onKeyPress={searchLocation} onChange={event => setLocation(event.target.value)}/>
-      
+  <Media queries={{
+    small: "(max-width: 599px)",
+    medium: "(min-width: 600px)"
+  }}>
+    {matches => (
+      <Fragment>
+        {matches.small && <div className={darkMode ? "searchlocations-container searchlocations-container-dark" : "searchlocations-container"}>
+
+
+<div className="search-container">
+
+<h1 className={darkMode ? "weathercam-h weathercam-h-dark" : "weathercam-h"}>WEATHER</h1>
+<h1 className={darkMode ? "weathercam-h weathercam-h-dark" : "weathercam-h"}>CAM</h1>
+<input id="searchlocations" className="searchlocations" type="text" placeholder="Search" aria-label="Search" value={location} onKeyPress={searchLocation} onChange={event => setLocation(event.target.value)}/>
+
+</div> 
+{
+  showdiv ? <div className={darkMode ? "widget-small  widget-small-dark" : "widget-small"}>
+  <div className="left-panel panel">
+      <div className={darkMode ? "date  date-dark" : "date"}>
+          <p>{daytime}</p>
       </div>
-        {
-          showdiv ? <div className={darkMode ? "widget  widget-dark" : "widget"}>
-          <div className="left-panel panel">
-              <div className={darkMode ? "date  date-dark" : "date"}>
-                  <p>{daytime}</p>
-              </div>
-              <div className={darkMode ? "city  city-dark" : "city"}>
-                  {data.name}
-              </div>
-              <div className={darkMode ? "temp  temp-dark" : "temp"}>
-                 <img src={weatherimage} alt="" width="100" height="100"/>
-                 {data.main && data.main.temp}&deg;
-              </div>
-          </div>
-          <div className="adetails">
-                <a className={darkMode ? "adetailstext  adetailstext-dark" : "adetailstext"} href={`/detailedcard/${data.name}`}>Details</a>
-          </div>
-    </div> : null
-        }
-      
-      
-      
-    </div>    
+      <div className={darkMode ? "city  city-dark" : "city"}>
+          {data.name}
+      </div>
+      <div className={darkMode ? "temp  temp-dark" : "temp"}>
+         <img src={weatherimage} alt="" width="100" height="100"/>
+         {data.main && data.main.temp}&deg;
+      </div>
+  </div>
+  <div className="adetails">
+        <a className={darkMode ? "adetailstext  adetailstext-dark" : "adetailstext"} href={`/detailedcard/${data.name}`}>Details</a>
+  </div>
+</div> : null
+}
+
+
+
+</div>    }
+        {matches.medium && <div className={darkMode ? "searchlocations-container searchlocations-container-dark" : "searchlocations-container"}>
+
+
+<div className="search-container">
+
+<h1 className={darkMode ? "weathercam-h weathercam-h-dark" : "weathercam-h"}>WEATHERCAM</h1>
+<input id="searchlocations" className="searchlocations" type="text" placeholder="Search" aria-label="Search" value={location} onKeyPress={searchLocation} onChange={event => setLocation(event.target.value)}/>
+
+</div>
+{
+  showdiv ? <div className={darkMode ? "widget  widget-dark" : "widget"}>
+  <div className="left-panel panel">
+      <div className={darkMode ? "date  date-dark" : "date"}>
+          <p>{daytime}</p>
+      </div>
+      <div className={darkMode ? "city  city-dark" : "city"}>
+          {data.name}
+      </div>
+      <div className={darkMode ? "temp  temp-dark" : "temp"}>
+         <img src={weatherimage} alt="" width="100" height="100"/>
+         {data.main && data.main.temp}&deg;
+      </div>
+  </div>
+  <div className="adetails">
+        <a className={darkMode ? "adetailstext  adetailstext-dark" : "adetailstext"} href={`/detailedcard/${data.name}`}>Details</a>
+  </div>
+</div> : null
+}
+
+
+
+</div>    }
+      </Fragment>
+    )}
+  </Media>
+
         
   )}
-
 
 
 
